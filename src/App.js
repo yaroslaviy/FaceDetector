ba9b3337df6295684c7076078c3cb917b9ac4511
yaroslaviy <yaselokeev@gmail.com>
2019-11-01 13:48:09 -0400

    finished GUI

--- src/App.js
+++ src/App.js
@@ -1,20 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Navigation from "./components/navigation/Navigation";
 import Logo from "./components/logo/logo";
 import ImageLinkForm from "./components/imagelinkform/ImageLinkForm";
+import Rank from "./components/rank/rank";
+import Particles from 'react-particles-js';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-       <Logo />
-       
-      <ImageLinkForm />
-      {/*
-      <FaceRecognition />} */}
-    </div>
-  );
+const particleparams = {
+    "particles": {
+        "number": {
+            "value": 50
+        },
+        "size": {
+            "value": 3
+        }
+    },
+    "interactivity": {
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
+            }
+        }
+    }
+};
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            input: ''
+        }
+    }
+
+    onInputChange = (event) => {
+      
+    }
+    onSubmitPicture = () => {
+      console.log('click');
+    }
+    render() {
+        return (
+            <div className="App">
+                <Particles className='particles' params={particleparams}/>
+                <Navigation/>
+                <Logo/>
+                <Rank/>
+                <ImageLinkForm onInputChange={this.onInputChange} onSubmitPicture={this.onSubmitPicture}/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/imagelinkform/ImageLinkForm.js
+++ src/components/imagelinkform/ImageLinkForm.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange , onSubmitPicture }) => {
     return ( 
         <div>
             <p className='f3'>
-                {'This will detect faces in your picture'}
+                {'This tool will detect faces in your picture. Give it a try'}
             </p>
-            <div className='center'>
-                <input className=' f4 pa2 w-70 center' type='text' />
-                <button className=' f4 link w-30 grow ph3 pv2 dib white bg-purple'>Detect</button>
+            <div className='center w-50 pa4 br3 shadow-5'>
+                <input className='f4 pa2 w-70' type='text' onChange={onInputChange} />
+                <button className='f4 link w-30 grow ph3 pv2 dib white bg-light-purple bn' onClick={onSubmitPicture}>Detect</button>
             </div>
         </div>
      );
