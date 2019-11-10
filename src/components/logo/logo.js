821ab13c647371df0bd05ed10806991d9af36767
yaroslaviy <yaselokeev@gmail.com>
2019-11-10 12:45:36 -0500

    changed logo

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Tilt from 'react-tilt';
 import 'tachyons';
 import './logo.css';
-import logo from './face-detection.png';
+import logo from './logo_new.jpg';
 
 const Logo = ()=>{
     return (
         <div className="ma4 mt0">
             <Tilt className="Tilt br2 shadow-2" options={{ max : 30 }} style={{ height: 150, width: 150 }} >
-                <div className="Tilt-inner"> <img  style ={{paddingTop: '10px'}} src={logo} alt='Logo'></img> </div>
+                <div className="Tilt-inner"> <img src={logo} alt='Logo'></img> </div>
             </Tilt>
         </div>
     );
